c8f7764b23b5ba275ab3bac6a14bf4415be8620f
Kumaresh <kumaresh.1597@gmail.com>
2023-08-18 16:28:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+//let elements = document.getElementsByTagName("ul");
+let element = document.getElementById("level");
+let word = element.innerText.split(" ");
+alert("The level of the element is: "+word[1]);
